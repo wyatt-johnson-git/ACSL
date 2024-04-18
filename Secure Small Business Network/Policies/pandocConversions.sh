8c24fef05321773cc194351f50019eb506acb59d
Andrew <github@blueshirt.dev>
2024-04-17 17:14:17 -0700

    appropriately limit the name regex for pandoc commands

--- Secure Small Business Network/Policies/pandocConversions.sh
+++ Secure Small Business Network/Policies/pandocConversions.sh
@@ -1,13 +1,13 @@
 # Uncomment the relevant command and run the script
 
 # docx to latex to markdown
-# find . -depth -name *.docx -exec sh -c 'pandoc {} -f docx -t latex | pandoc -f latex -t markdown -o {}.md' \;
+# find . -depth -name "*.docx" -exec sh -c 'pandoc "{}" -f docx -t latex | pandoc -f latex -t markdown -o "{}".md' \;
 
 # docx to markdown
-# find . -depth -name *.docx -exec sh -c 'pandoc {} -f docx -t markdown -o {}.md' \;
+# find . -depth -name "*.docx" -exec sh -c 'pandoc "{}" -f docx -t markdown -o "{}".md' \;
 
 # markdown to docx
-find . -depth -name *.md -exec sh -c 'pandoc {} -f markdown -t docx -o {}.md' \;
+# find . -depth -name "*.md" -exec sh -c 'pandoc "{}" -f markdown -t docx -o "{}".docx' \;
 
 # markdown to latex to docx
-# find . -depth -name *.md -exec sh -c 'pandoc {} -f markdown -t latex | pandoc -f latex -t docx -o {}.md' \;
+find . -depth -name "*.md" -exec sh -c 'pandoc "{}" -f markdown -t latex | pandoc -f latex -t docx -o "{}".tex.docx' \;
